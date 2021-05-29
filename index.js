ad48741aeeebcc6ad41e8671475462053845ac8b
AnOrdinaryUsername <kylemasa2@gmail.com>
2021-05-28 17:41:14 -0700

    refactor: Utilize Express for easier development

--- index.js
+++ index.js
@@ -1,81 +1,25 @@
-const http = require("http");
-const fs = require("fs");
+const { response } = require("express");
+const express = require("express");
+const path = require("path");
+const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-http
-  .createServer((request, response) => {
-    const url = new URL(request.url, `https://${request.headers.host}`);
-    let path = url.pathname;
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/pages/index.html"));
+});
 
-    if (path.indexOf(".css") !== -1) {
-      grabRequestedFile("pages/styles.css", "text/css", response);
-    } else {
-      grabRequestedFile(path, "text/html", response);
-    }
-  })
-  .listen(PORT);
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "/pages/about.html"));
+});
 
-function grabRequestedFile(pathName, mimeType, response) {
-  let pagePath = null;
-  if (mimeType === "text/html") {
-    switch (pathName) {
-      case "/":
-        pagePath = "pages/index.html";
-        break;
-      case "/about":
-        pagePath = "pages/about.html";
-        break;
-      case "/contact-me":
-        pagePath = "pages/contact-me.html";
-        break;
-      default:
-        pagePath = "pages/404.html";
-        break;
-    }
-  } else {
-    pagePath = pathName;
-  }
+app.get("/contact-me", (req, res) => {
+  res.sendFile(path.join(__dirname, "/pages/contact-me.html"));
+});
 
-  fs.readFile(pagePath, (error, data) => {
-    switch (mimeType) {
-      case "text/html":
-        if (error) {
-          show404Page(response);
-          return;
-        }
+app.get("/styles.css", (req, res) => {
+  res.sendFile(path.join(__dirname, "/pages/styles.css"));
+});
 
-        response.writeHead(200, { "Content-Type": "text/html" });
-        response.write(data);
-
-        response.end();
-        break;
-      case "text/css":
-        if (error) {
-          throw new Error(error);
-        }
-
-        response.writeHead(200, { "Content-Type": "text/css" });
-        response.write(data);
-        response.end();
-
-        break;
-      default:
-        throw new Error(`${mimeType} does not exist`);
-    }
-  });
-}
-
-function show404Page(response) {
-  const errorPagePath = "pages/404.html";
-
-  fs.readFile(errorPagePath, (error, data) => {
-    if (error) {
-      throw new Error(`${errorPagePath} does not exist: ${error}`);
-    }
-
-    response.writeHead(404, { "Content-Type": "text/html" });
-    response.write(data);
-    response.end();
-  });
-}
+app.use(express.static("pages"));
+app.listen(PORT);
